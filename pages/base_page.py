1a36e0682762b823be249c95a2e0f30c43a71ee8
Dashanikusha <dasha.nikulina.1991@gmail.com>
2022-08-11 19:12:37 +0200

    added Page Object for login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,3 +4,13 @@ class BasePage():
         self.url = url
     def open(self):
         self.browser.get(self.url)
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+    def is_element_present(self, css, id):
+        try:
+            self.browser.find_element(css, id)
+        except (NoSuchElementException):
+            return False
+        return True
